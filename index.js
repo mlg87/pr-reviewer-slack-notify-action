13e8ffd1531eba3d83fbe65b97cc8a230ec1c882
Mason Goetz <masonlgoetz@gmail.com>
2020-07-24 15:40:31 -0600

    updated action to take slack users array

--- index.js
+++ index.js
@@ -2,11 +2,10 @@ const core = require("@actions/core");
 const github = require("@actions/github");
 
 try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput("who-to-greet");
-  console.log(`Hello ${nameToGreet}!`);
-  const time = new Date().toTimeString();
-  core.setOutput("time", time);
+  // `slack-users` input defined in action metadata file
+  const slackUsers = JSON.parse(core.getInput("slack-users"));
+  console.log("slackUsers", slackUsers);
+
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2);
   console.log(`The event payload: ${payload}`);
