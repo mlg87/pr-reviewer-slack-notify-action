d5a620591b22281f23da206481812e7dc35b8d73
Mason Goetz <masonlgoetz@gmail.com>
2020-07-24 15:57:14 -0600

    attempt to make request

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
+const fetch = require("node-fetch");
 
-try {
-  // `slack-users` input defined in action metadata file
-  const slackUsers = JSON.parse(core.getInput("slack-users"));
-  console.log("slackUsers", slackUsers);
+(() => {
+  try {
+    // `slack-users` input defined in action metadata file
+    const slackUsers = JSON.parse(core.getInput("slack-users"));
+    // Get the JSON webhook payload for the event that triggered the workflow
+    const payload = JSON.stringify(github.context.payload, undefined, 2);
 
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2);
-  console.log(`The event payload: ${payload}`);
-} catch (error) {
-  core.setFailed(error.message);
-}
+    const options = {
+      body: JSON.stringify({
+        text: `${payload.sender.login} is requesting your review on ${payload.pull_request._links.html.href}`,
+      }),
+      headers: { "Content-Type": "application/json" },
+      method: "POST",
+    };
+    slackUsers.forEach(async (user) => {
+      await fetch(user.slack_webhook, options);
+    });
+
+    console.log(`The event payload: ${payload}`);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+})();
