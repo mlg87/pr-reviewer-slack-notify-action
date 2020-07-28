1939e3d0ad126b28858f23116e5e38d1549576e4
Mason Goetz <masonlgoetz@gmail.com>
2020-07-28 13:00:43 -0600

    remove DM feature in favor of spamming channel, update README, make channel-webhook a required input

--- index.js
+++ index.js
@@ -4,58 +4,33 @@ const fetch = require("node-fetch");
 
 (async () => {
   try {
-    const actionType = github.context.payload.action;
-    console.log("actionType", actionType);
-
     const channelWebhook = core.getInput("channel-webhook");
-    // `slack-users` input defined in action metadata file
     const slackUsers = JSON.parse(core.getInput("slack-users"));
-    // Get the JSON webhook payload for the event that triggered the workflow
-    // const payload = JSON.stringify(github.context.payload, undefined, 2);
-    // console.log("payload", payload);
     const requestedReviewers = github.context.payload.pull_request.requested_reviewers.map(
       (user) => user.login
     );
-    console.log("requested_reviewers", requestedReviewers);
-
     const baseMessage = `${github.context.payload.sender.login} is requesting your review on ${github.context.payload.pull_request._links.html.href}`;
 
-    // if there is no channelWebhook specified, DM the requested reviewers
-    if (!channelWebhook) {
-      const options = {
-        body: JSON.stringify({
-          text: baseMessage,
-        }),
-        headers: { "Content-Type": "application/json" },
-        method: "POST",
-      };
-      slackUsers.forEach(async (user) => {
-        if (requestedReviewers.includes(user.github_username)) {
-          await fetch(user.slack_webhook, options);
-        }
-      });
-    } else {
-      const usersToAt = slackUsers.filter((user) =>
-        requestedReviewers.includes(user.github_username)
-      );
-      let usersToAtString;
-      usersToAt.forEach((user) => {
-        if (!usersToAtString) {
-          usersToAtString = `<@${user.slack_id}>`;
-          return;
-        }
-        usersToAtString = `${usersToAtString}, <@${user.slack_id}>`;
+    const usersToAt = slackUsers.filter((user) =>
+      requestedReviewers.includes(user.github_username)
+    );
+    let usersToAtString;
+    usersToAt.forEach((user) => {
+      if (!usersToAtString) {
+        usersToAtString = `<@${user.slack_id}>`;
         return;
-      });
-      const options = {
-        body: JSON.stringify({
-          text: `${usersToAtString} ${baseMessage}`,
-        }),
-        headers: { "Content-Type": "application/json" },
-        method: "POST",
-      };
-      await fetch(channelWebhook, options);
-    }
+      }
+      usersToAtString = `${usersToAtString}, <@${user.slack_id}>`;
+      return;
+    });
+    const options = {
+      body: JSON.stringify({
+        text: `${usersToAtString} ${baseMessage}`,
+      }),
+      headers: { "Content-Type": "application/json" },
+      method: "POST",
+    };
+    await fetch(channelWebhook, options);
   } catch (error) {
     core.setFailed(error.message);
   }
