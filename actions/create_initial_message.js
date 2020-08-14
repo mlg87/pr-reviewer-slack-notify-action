843a83ffc9a463137a76bf4c0a5eaa1f8099fd81
Mason Goetz <masonlgoetz@gmail.com>
2020-08-14 14:22:48 -0600

    updated initial message handler

--- actions/create_initial_message.js
+++ actions/create_initial_message.js
@@ -1,38 +1,40 @@
-module.export = async () => {
+const core = require("@actions/core");
+const { slackWebClient } = require("../utils/slackWebClient");
 
-    try {
-        const channelWebhook = core.getInput("channel-webhook");
-        const slackUsers = JSON.parse(core.getInput("slack-users"));
-        const requestedReviewers = github.context.payload.pull_request.requested_reviewers.map(
-          (user) => user.login
-        );
-        if (!requestedReviewers.length) {
-          return null;
-        }
-        const baseMessage = `${github.context.payload.sender.login} is requesting your review on ${github.context.payload.pull_request._links.html.href}`;
-    
-        const usersToAt = slackUsers.filter((user) =>
-          requestedReviewers.includes(user.github_username)
-        );
-        let usersToAtString;
-        usersToAt.forEach((user) => {
-          if (!usersToAtString) {
-            usersToAtString = `<@${user.slack_id}>`;
-            return;
-          }
-          usersToAtString = `${usersToAtString}, <@${user.slack_id}>`;
-          return;
-        });
-        const options = {
-          body: JSON.stringify({
-            text: `${usersToAtString} ${baseMessage}`,
-          }),
-          headers: { "Content-Type": "application/json" },
-          method: "POST",
-        };
-        await fetch(channelWebhook, options);
-      } catch (error) {
-        core.setFailed(error.message);
+module.export = async () => {
+  try {
+    const channelId = core.getInput("channel-id");
+    const slackUsers = JSON.parse(core.getInput("slack-users"));
+    const requestedReviewers = github.context.payload.pull_request.requested_reviewers.map(
+      (user) => user.login
+    );
+    // stop everything if there are no requested reviewers
+    if (!requestedReviewers.length) {
+      return null;
+    }
+    // TODO update this to include content from the author's description or last comment
+    const baseMessage = `${github.context.payload.sender.login} is requesting your review on ${github.context.payload.pull_request._links.html.href}`;
+
+    // build users to mention string
+    const usersToAt = slackUsers.filter((user) =>
+      requestedReviewers.includes(user.github_username)
+    );
+    let usersToAtString;
+    usersToAt.forEach((user) => {
+      if (!usersToAtString) {
+        usersToAtString = `<@${user.slack_id}>`;
+        return;
       }
-      
-}
+      usersToAtString = `${usersToAtString}, <@${user.slack_id}>`;
+      return;
+    });
+    // TODO save ids to artifact here
+    // See: https://api.slack.com/methods/chat.postMessage
+    return await slackWebClient.chat.postMessage({
+      channel: channelId,
+      text: `${usersToAtString} ${baseMessage}`,
+    });
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+};
---
+++ utils/slackWebClient.js
@@ -0,0 +1,9 @@
+const core = require("@actions/core");
+const { WebClient } = require("@slack/web-api");
+
+const token = core.getInput("bot-token");
+const slackWebClient = new WebClient(token);
+
+module.exports = {
+  slackWebClient,
+};
