7e9fa1fdde92b2894aed5a094c501d627c8ba86d
Mason Goetz <masonlgoetz@gmail.com>
2020-08-14 14:29:03 -0600

    fix weird autoformatting

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const core = require("@actions/core");
 const github = require("@actions/github");
 const fetch = require("node-fetch");
 
-const createInitialMessage = require("./actions/createInitialMessage")(
-  async () => {
-    // route to the appropriate action
-  }
-)();
+const createInitialMessage = require("./actions/createInitialMessage");
+
+(async () => {
+  // route to the appropriate action
+})();
