2a3aa2ab9fc453c0aac61a4ca6ce6086338264de
Mason Goetz <masonlgoetz@gmail.com>
2020-08-14 15:10:31 -0600

    add log

--- index.js
+++ index.js
@@ -6,11 +6,14 @@ const { createInitialMessage } = require("./actions");
 
 (async () => {
   const { payload } = github.context;
+  console.log("github.context", github.context);
+
   // route to the appropriate action
   // triggered by one of the pull_request events (opened, ready_for_review, merged)
   if (!!payload.pull_request) {
     if (payload.action === "opened" || payload.action === "ready_for_review") {
       const res = await createInitialMessage();
+      // TODO store the message id and the PR number in the artifact
       console.log("init msg res", res);
     } else if (payload.action === "merged") {
       // TODO add handler for closing out the process here
