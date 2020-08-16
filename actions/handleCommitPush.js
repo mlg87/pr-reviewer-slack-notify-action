b07b0ca60400e66fa3333b48b707b1541befcb70
Mason Goetz <masonlgoetz@gmail.com>
2020-08-15 20:57:08 -0600

    attempt to retrieve PRs with commit

--- actions/handleCommitPush.js
+++ actions/handleCommitPush.js
@@ -8,44 +8,38 @@ const { getSlackMessageId } = require("../utils");
 // explicitly asks for a re-review
 module.exports = async () => {
   try {
-    console.log("in handle commit push");
-    console.log("github.context.payload", github.context.payload);
-    console.log("shouldve logged the github context payload");
-
     const channelId = core.getInput("channel-id");
     const ghToken = core.getInput("github-token");
     const slackUsers = JSON.parse(core.getInput("slack-users"));
+    const requestedReviewers = github.context.payload.pull_request.requested_reviewers.map(
+      (user) => user.login
+    );
+    const { commits, repository } = github.context.payload;
+    //
+    // ─── GET THE ISSUE NUMBER FOR THE COMMIT ─────────────────────────
+    //
+    const prRes = await fetch(
+      `https://api.github.com/${repository.full_name}/commits/${commits[0].id}/pulls`,
+      {
+        headers: {
+          Authorization: `token ${ghToken}`,
+        },
+        method: "GET",
+      }
+    );
 
-    return null;
+    console.log("prRes", prRes);
+    throw Error("not implemented yet");
 
-    // const requestedReviewers = github.context.payload.pull_request.requested_reviewers.map(
-    //   (user) => user.login
-    // );
-    // const { repository } = github.context.payload;
-    // //
-    // // ─── GET THE ISSUE NUMBER FOR THE COMMIT ─────────────────────────
-    // //
+    const slackMessageId = await getSlackMessageId();
+    //
+    // ─── CLEAR ALL REACTIONS BC THERE IS NEW CODE ────────────────────
+    //
 
-    // // TODO pick up here: add SHA to request
-    // const prRes = await fetch(
-    //   `https://api.github.com/${repository.full_name}/commits/${}/pulls`,
-    //   {
-    //     headers: {
-    //       Authorization: `token ${ghToken}`
-    //     },
-    //     method: 'GET'
-    //   }
-    // )
-
-    // const slackMessageId = await getSlackMessageId()
-    // //
-    // // ─── CLEAR ALL REACTIONS BC THERE IS NEW CODE ────────────────────
-    // //
-
-    // const existingReactions = await slackWebClient.reactions.get({
-    //   channel: channelId,
-    //   timestamp: slackMessageId,
-    // });
+    const existingReactions = await slackWebClient.reactions.get({
+      channel: channelId,
+      timestamp: slackMessageId,
+    });
 
     //
     // ─── NOTIFY REVIEWERS IN THREAD ──────────────────────────────────
